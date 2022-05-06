9eaf5c670786213cd3cb019bfc29f1587c1c8ed7
Lucho-lja <ljallauca.a@gmail.com>
2022-05-06 18:00:08 -0500

    Add user and post model with 1 data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    has_many :posts
+    
     validates :name, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true
     validates :age, presence: true
